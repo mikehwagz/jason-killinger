c5fed0efec5a5d4cc18e79d4ca22791891f283ce
mikehwagz <mike@selfaware.studio>
2021-09-02 09:47:58 -0400

    update netlify dashboard widget

--- studio/parts/dashboard.js
+++ studio/parts/dashboard.js
@@ -6,10 +6,10 @@ export default {
         title: 'Netlify',
         sites: [
           {
-            title: '<SITE_TITLE>',
-            apiId: '<NETLIFY_API_ID>',
-            buildHookId: '<NETLIFY_BUILD_HOOK_ID>',
-            name: '<NETLIFY_SITE_NAME>',
+            title: 'Production',
+            apiId: 'd07c2b4e-aee4-41b9-aad7-2d8987603c34',
+            buildHookId: '6130d5e84b2d41dacaceeca7',
+            name: 'jasonkillinger',
           },
         ],
       },
