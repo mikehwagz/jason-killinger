c4153365617a06def12fe9dd88b8081f11a19991
mikehwagz <mike@selfaware.studio>
2021-09-09 14:20:55 -0400

    optimize header theme change for case study

--- scripts/components/caseStudy.js
+++ scripts/components/caseStudy.js
@@ -1,20 +1,22 @@
 import choozy from 'choozy'
-import { rect } from 'martha'
+import { qs, rect } from 'martha'
 import { component } from 'picoapp'
 
 export default component((node, ctx) => {
   const { hero } = choozy(node)
+  const header = qs('[data-scroll-padding-top]')
   const theme = hero.dataset.theme
   const defaultTheme = 'black'
 
   let last = null
 
   ctx.on('resize', () => {
+    header.rect = rect(header)
     hero.rect = rect(hero)
   })
 
   ctx.on('tick', ({ scroll }) => {
-    if (scroll.target <= hero.rect.height) {
+    if (scroll.target <= hero.rect.height - header.rect.height * 0.5) {
       if (last !== theme) {
         ctx.emit('header:theme', null, theme)
         last = theme
