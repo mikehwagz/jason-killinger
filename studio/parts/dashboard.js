7d34a141befc2fc62f5a4804ee78dadd8a307754
mikehwagz <mike@selfaware.studio>
2021-10-26 14:22:40 -0400

    update netlify hook in sanity dashboard

--- studio/parts/dashboard.js
+++ studio/parts/dashboard.js
@@ -7,8 +7,8 @@ export default {
         sites: [
           {
             title: 'Production',
-            apiId: 'd07c2b4e-aee4-41b9-aad7-2d8987603c34',
-            buildHookId: '6130d5e84b2d41dacaceeca7',
+            apiId: '36b2879a-8815-467f-9a19-196626719c60',
+            buildHookId: '6178474154b2258498bf760b',
             name: 'jasonkillinger',
           },
         ],
